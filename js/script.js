15c79104c7c328905572fd525715b488b91cce15
george <georgekarani77@gmail.com>
2019-06-10 09:57:31 +0300

    add function to make logo icons to show text and hide image.

--- js/script.js
+++ js/script.js
@@ -6,8 +6,14 @@ $(function(){
 });
 
 $(document).ready(function () {
-  // delani
+  //////////// delani stdio//////////////
   $("#delani").fadeOut(1000);
   $("#head").addClass("onLoad").fadeIn(2000);
   $("html, body").scrollTop(0);
 
+ ////////////logo icons effect////////////////
+  $(".text").hide();
+  $(".whatDo").click(function () {
+    $(this).children(".text").slideToggle(500);
+    $(this).children(".hide-img").fadeToggle(500);
+  });
