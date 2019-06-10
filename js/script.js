613a0a08fa636d2f1972a8dc3ac63aaf7d3df717
george <georgekarani77@gmail.com>
2019-06-10 10:00:07 +0300

    add function to make return message received and error with no text.

--- js/script.js
+++ js/script.js
@@ -26,3 +26,20 @@ $(document).ready(function () {
   $(".port-asc").mouseleave(function () {
     $(this).children(".port_desc").removeClass("here").fadeIn;
   });
+
+  // form effect
+  $("#submission").submit(function(event){
+    var data_1 = $("input:first").val();
+    var data_2 = $("input#mail").val();
+    if(data_1 && data_2 != ""  ){
+      $(".message").text("Thank you " + data_1 + "message received");
+      $(".message").removeClass("error");
+      $(".message").addClass("success");
+    }else{
+      $(".message").text("Error!! Please fill in all the details correctly");
+      $(".message").addClass("error");
+    }
+    event.preventDefault();
+  });
+
+});
